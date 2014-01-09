000921687929e479ad31ba6e601cb228d382600f
crazypoo <273277355@qq.com>
2014-01-09 17:29:43 +0800

    Initial Commit

---
+++ no/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  no
+//
+//  Created by crazypoo on 1/9/14.
+//  Copyright (c) 2014 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
