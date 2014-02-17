578de84938b96aa36907cdac6c3050a5d39c4c1b
crazypoo <273277355@qq.com>
2014-02-17 17:22:33 +0800

    BarNotification
    BarNotification

--- no/ViewController.m
+++ no/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "JDStatusBarNotification.h"
 @interface ViewController ()
 
 @end
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [JDStatusBarNotification showWithStatus:@"Hello World!" dismissAfter:2.0
+                                  styleName:JDStatusBarStyleMatrix];
+
 }
 
 - (void)didReceiveMemoryWarning
